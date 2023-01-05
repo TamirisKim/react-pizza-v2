ef09955be0c3bd529b7f3c873363046d23b24c7f
TamirisKim <kimbaevat@gmail.com>
2023-01-05 18:48:55 +0600

    11-les useContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,11 @@ import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import { Pagination } from "../components/Pagination";
+import { SearchContext } from "../App";
 
-export const Home = ({ searchValue }) => {
+export const Home = () => {
+  const {searchValue} = React.useContext(SearchContext)
+
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [currentPage, setCurrentPage] = React.useState(1);
