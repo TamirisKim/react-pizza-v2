3b887bcf8c2b84eeb72f377c2325deef58a6a519
TamirisKim <kimbaevat@gmail.com>
2023-01-05 16:49:40 +0600

    10-les поиск тов с бэк, пагинация

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,12 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
+import { Pagination } from "../components/Pagination";
 
 export const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [currentPage, setCurrentPage] = React.useState(1);
 
   const [categoryId, setCategoryId] = React.useState(0);
   const [sortType, setSortType] = React.useState({
@@ -21,9 +23,10 @@ export const Home = ({ searchValue }) => {
     const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
     const sortBy = sortType.sortProperty.replace("-", "");
     const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue > 0 ? `&search=${searchValue}` : "";
 
     fetch(
-      `https://6399e68f16b0fdad774d67dc.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://6399e68f16b0fdad774d67dc.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -31,7 +34,9 @@ export const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+
 
   const pizzas = items.filter((obj) => {
     if (obj.title.toLowerCase().includes(searchValue.toLowerCase())){
@@ -57,6 +62,7 @@ export const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skelenos : pizzas}</div>
+      <Pagination onChangePage={number => setCurrentPage(number)} currentPage={currentPage}/>
     </div>
   );
 };
